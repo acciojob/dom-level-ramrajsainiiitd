90ba88d8dd644bc038e5f59cf5c56cd4d17111c3
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-16 16:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function LevelOfElement() {
+  let node = document.getElementById('level'); // Fix variable name 'ChildNode' to 'node'
+  let count = -1;
+  while (node != null) { // Fix variable name 'ChildNode' to 'node' and add condition for loop termination
+    count += 1; // Increment count inside the loop
+    //console.log(count, node);
+    node = node.parentNode; // Update 'ChildNode.parentNode' to 'node.parentNode' and assign the result to 'node'
+  }
+  window.alert(`The level of the element is: ${count}`);
+}
+console.log(LevelOfElement());
